e351d01c8545ca3d1c5dc51dbdb00fd518323888
Christopher Ball <chris.m.ball@gmail.com>
2024-07-19 17:25:02 -0700

    Removed furigana invisibility hack in place or cleaner approach.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,84 @@
-document
-    .getElementById("generateButton")
-    .addEventListener("click", function () {
-        const inputText = document.getElementById("inputText").value;
-        generateFurigana(inputText);
+function isKanji(character) {
+    let regex = /[\u4e00-\u9faf\u3400-\u4dbf]/;
+    return regex.test(character);
+}
+
+function splitToken(surfaceForm, readingHiragana) {
+    let lastHiraganaIndex = null;
+    let extractedHiraganaChunk = "";
+    let outputHtml = "";
+
+    [...surfaceForm].forEach((c, index) => {
+        if (wanakana.isHiragana(c)) {
+            if (lastHiraganaIndex == null || index == lastHiraganaIndex + 1) {
+                extractedHiraganaChunk += c;
+                lastHiraganaIndex = index;
+            }
+            // This is the rare bail case where our token has scattered hiragana
+            else if (index > lastHiraganaIndex + 1) {
+                return `<ruby>${surfaceForm}<rt>${readingHiragana}</rt></ruby>`;
+            }
+        }
     });
+
+    let extractedHiraganaChunkStartIndex = surfaceForm.indexOf(
+        extractedHiraganaChunk
+    );
+
+    // Covering case where hiragana is at beginning of token
+    if (extractedHiraganaChunkStartIndex == 0) {
+        outputHtml += extractedHiraganaChunk;
+        outputHtml +=
+            "<ruby>" +
+            surfaceForm.substring(extractedHiraganaChunk.length) +
+            "<rt>" +
+            readingHiragana.replace(extractedHiraganaChunk, "") +
+            "</rt></ruby>";
+    }
+    // Covering case where hiragana is in the middle of token
+    else if (
+        extractedHiraganaChunkStartIndex > 0 &&
+        extractedHiraganaChunkStartIndex + extractedHiraganaChunk.length <
+            surfaceForm.length
+    ) {
+        let re = new RegExp(extractedHiraganaChunk + ".+", "g");
+        let re2 = new RegExp(".+" + extractedHiraganaChunk, "g");
+        let surfChunk1 = surfaceForm.substring(
+            0,
+            extractedHiraganaChunkStartIndex
+        );
+        let surfChunk2 = extractedHiraganaChunk;
+        let surfChunk3 = surfaceForm.substring(
+            surfChunk1.length + surfChunk2.length
+        );
+
+        outputHtml +=
+            "<ruby>" +
+            surfChunk1 +
+            "<rt>" +
+            readingHiragana.replace(re, "") +
+            "</rt></ruby>";
+        outputHtml += surfChunk2;
+        outputHtml +=
+            "<ruby>" +
+            surfChunk3 +
+            "<rt>" +
+            readingHiragana.replace(re2, "") +
+            "</rt></ruby>";
+    }
+    // Covering case where hiragana is at end of token
+    else {
+        outputHtml +=
+            "<ruby>" +
+            surfaceForm.replace(extractedHiraganaChunk, "") +
+            "<rt>" +
+            readingHiragana.replace(extractedHiraganaChunk, "") +
+            "</rt></ruby>";
+        outputHtml += extractedHiraganaChunk;
+    }
+
+    return outputHtml;
+}
 
 function generateFurigana(text) {
     kuromoji
@@ -22,27 +97,11 @@ function generateFurigana(text) {
             tokens.forEach((token) => {
                 let readingHiragana = wanakana.toHiragana(token.reading);
 
-                // Where safely possible, hiding repetitive hiragana in readings
-                // that are part of the mixed kanji/hiragana token.
-                if (!wanakana.isHiragana(token.surface_form)) {
-                    [...token.surface_form].forEach((c) => {
-                        let re = new RegExp("[^" + c + "]", "g");
-                        if (
-                            wanakana.isHiragana(c) &&
-                            readingHiragana.replace(re, "").length == 1
-                        ) {
-                            readingHiragana = readingHiragana.replace(
-                                c,
-                                '<span style="visibility:hidden;">' +
-                                    c +
-                                    "</span>"
-                            );
-                        }
-                    });
-                }
-
                 if (token.surface_form !== readingHiragana) {
-                    outputHtml += `<ruby>${token.surface_form}<rt>${readingHiragana}</rt></ruby>`;
+                    outputHtml += splitToken(
+                        token.surface_form,
+                        readingHiragana
+                    );
                 } else {
                     outputHtml += token.surface_form;
                 }
@@ -52,3 +111,14 @@ function generateFurigana(text) {
             document.getElementById("outputHTML").value = outputHtml;
         });
 }
+
+function main() {
+    document
+        .getElementById("generateButton")
+        .addEventListener("click", function () {
+            const inputText = document.getElementById("inputText").value;
+            generateFurigana(inputText);
+        });
+}
+
+window.onload = main();
